99c2ee61a456a4559bb627bd9796486130f9104f
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-01-17 12:54:30 +0100

    WIP

---
+++ ClientConsole1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClientConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new MyLibrary.MyService1(Console.Out);
+
+            x.WriteMessage("This is cool.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyLibrary/MyService1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace MyLibrary
+{
+    public class MyService1
+    {
+        private readonly TextWriter writer;
+
+        public MyService1(TextWriter writer)
+        {
+            this.writer = writer ?? Console.Out;
+
+        }
+
+        public void WriteMessage(string message)
+        {
+            this.writer.WriteLine(message);
+        }
+    }
+}
